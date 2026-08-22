caa3558b259fb27658a8e61f07569edec3c94dcd
scott obiaya <scott@obiaya.co.uk>
2026-08-23 00:06:28 +0100

    test add role_jenkins

--- main.tf
+++ main.tf
@@ -569,7 +569,42 @@ resource "aws_autoscaling_group" "jenkins" {
 }
 
 
+resource "aws_iam_role" "jenkins-role" {
+  name = "jenkins_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "Pro-Jenkins"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "jenkins_admin" {
+  role       = aws_iam_role.jenkins-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
 resource "aws_iam_role_policy_attachment" "jenkins_ssm" {
   role       = aws_iam_role.jenkins-role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "jenkins_profile" {
+  name = "jenkin_profile"
+
+  role = aws_iam_role.jenkins-role.name
 }
