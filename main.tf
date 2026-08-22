fb091318c0188c9162bd08780fe5dd4c2807f61c
scott obiaya <scott@obiaya.co.uk>
2026-08-22 22:52:37 +0100

    Added map

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ resource "aws_subnet" "sub1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Pro"
@@ -21,6 +22,7 @@ resource "aws_subnet" "sub2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Pro"
