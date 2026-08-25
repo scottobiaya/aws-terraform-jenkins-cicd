0946d3019139c10d68d10013326063b263e3ac35
scott obiaya <scott@obiaya.co.uk>
2026-08-25 23:28:01 +0100

    updated main and removed bootstrap

--- main.tf
+++ main.tf
@@ -220,59 +220,6 @@ resource "aws_autoscaling_group" "bar" {
   }
 }
 
-resource "aws_iam_role" "jenkins-role" {
-  name = "jenkins_role"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  tags = {
-    Name = "Pro-Jenkins"
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "jenkins_admin" {
-  role       = aws_iam_role.jenkins-role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-
-# resource "aws_iam_role_policy" "jenkins_ssm" {
-#   name = "jenkins-ssm-policy"
-#   role = aws_iam_role.jenkins-role.id
-
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-
-#     Statement = [
-#       {
-#         Effect = "Allow"
-
-#         Action = [
-#           "ssm:SendCommand",
-#           "ssm:GetCommandInvocation",
-#           "ssm:ListCommandInvocations",
-#           "ssm:DescribeInstanceInformation"
-#         ]
-
-#         Resource = "*"
-#       }
-#     ]
-#   })
-# }
 
 
 
@@ -454,98 +401,104 @@ resource "aws_launch_template" "Pro-jenkins" {
       aws_security_group.jenkins-sg.id
     ]
   }
+user_data = base64encode(<<-EOF
+  #!/bin/bash
+  set -e
 
-  user_data = base64encode(<<-EOF
-    #!/bin/bash
-    set -e
+  echo "===== Starting Jenkins installation ====="
 
-    echo "===== Starting Jenkins installation ====="
+  sed -i 's|http://us-east-1.ec2.archive.ubuntu.com/ubuntu/|http://archive.ubuntu.com/ubuntu/|g' /etc/apt/sources.list.d/ubuntu.sources
 
-    echo "Updating packages..."
-    apt update -y
+  echo "Updating packages..."
+  apt update -y
 
-    echo "Installing required packages..."
-    apt install -y \
-      wget \
-      awscli \
-      fontconfig \
-      openjdk-21-jre \
-      docker.io \
-      unzip
+  echo "Installing required packages..."
+  apt install -y \
+    wget \
+    awscli \
+    fontconfig \
+    openjdk-21-jre \
+    docker.io \
+    unzip \
+    snapd
 
-    echo "Starting Docker..."
-    systemctl enable --now docker
+  echo "Starting Docker..."
+  systemctl enable --now docker
 
-    echo "Adding Jenkins repository key..."
-    mkdir -p /etc/apt/keyrings
+  echo "Installing and starting Amazon SSM Agent..."
+  snap install amazon-ssm-agent --classic
+  systemctl enable --now snap.amazon-ssm-agent.amazon-ssm-agent.service
 
-    wget -O /etc/apt/keyrings/jenkins-keyring.asc \
-      https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+  echo "Adding Jenkins repository key..."
+  mkdir -p /etc/apt/keyrings
 
-    echo "Adding Jenkins repository..."
-    echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" \
-      > /etc/apt/sources.list.d/jenkins.list
+  wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 
-    echo "Updating package lists..."
-    apt update -y
+  echo "Adding Jenkins repository..."
+  echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" \
+    > /etc/apt/sources.list.d/jenkins.list
 
-    echo "Installing Jenkins..."
-    apt install -y jenkins
+  echo "Updating package lists..."
+  apt update -y
 
-    echo "Installing Terraform..."
+  echo "Installing Jenkins..."
+  apt install -y jenkins
 
-    TERRAFORM_VERSION="1.15.9"
+  echo "Installing Terraform..."
 
-    wget -q \
-      "https://releases.hashicorp.com/terraform/$${TERRAFORM_VERSION}/terraform_$${TERRAFORM_VERSION}_linux_amd64.zip" \
-      -O /tmp/terraform.zip
+  TERRAFORM_VERSION="1.15.9"
 
-    unzip -o /tmp/terraform.zip -d /usr/local/bin/
+  wget -q \
+    "https://releases.hashicorp.com/terraform/$${TERRAFORM_VERSION}/terraform_$${TERRAFORM_VERSION}_linux_amd64.zip" \
+    -O /tmp/terraform.zip
 
-    chmod +x /usr/local/bin/terraform
+  unzip -o /tmp/terraform.zip -d /usr/local/bin/
 
-    rm -f /tmp/terraform.zip
+  chmod +x /usr/local/bin/terraform
 
-    echo "Adding Jenkins user to Docker group..."
-    usermod -aG docker jenkins
+  rm -f /tmp/terraform.zip
 
-    echo "Enabling Jenkins..."
-    systemctl enable jenkins
+  echo "Adding Jenkins user to Docker group..."
+  usermod -aG docker jenkins
 
-    echo "Starting Jenkins..."
-    systemctl start jenkins
+  echo "Enabling Jenkins..."
+  systemctl enable jenkins
 
-       echo "Checking installations..."
-    echo "===== Terraform ====="
-    terraform version
+  echo "Starting Jenkins..."
+  systemctl start jenkins
 
-    echo "===== AWS CLI ====="
-    aws --version
+  echo "Checking installations..."
+  echo "===== Terraform ====="
+  terraform version
 
-    echo "===== Docker ====="
-    docker --version
+  echo "===== AWS CLI ====="
+  aws --version
 
-    echo "===== Java ====="
-    java -version
+  echo "===== Docker ====="
+  docker --version
 
-    echo "===== Jenkins ====="
-    systemctl status jenkins --no-pager || true
+  echo "===== Java ====="
+  java -version
 
-    echo "===== Jenkins installation completed ====="
-  EOF
-  )
+  echo "===== Jenkins ====="
+  systemctl status jenkins --no-pager || true
 
-  key_name = var.key-pair
+  echo "===== SSM Agent ====="
+  systemctl status snap.amazon-ssm-agent.amazon-ssm-agent.service --no-pager || true
 
-  iam_instance_profile {
-    name = data.aws_iam_instance_profile.jenkins_profile.name
-  }
+  echo "===== Jenkins installation completed ====="
+EOF
+)
 
+key_name = var.key-pair
+
+iam_instance_profile {
+  name = aws_iam_instance_profile.jenkins_profile.name
+}
 }
 
-data "aws_iam_instance_profile" "jenkins_profile" {
-  name = "jenkin_profile"
-}
+
 
 resource "aws_autoscaling_group" "jenkins" {
   desired_capacity = 1
@@ -566,38 +519,36 @@ resource "aws_autoscaling_group" "jenkins" {
   }
 }
 
+resource "aws_iam_role" "jenkins-role" {
+  name = "jenkins_role"
 
-
-# resource "aws_iam_instance_profile" "jenkins_profile" {
-#   name = "jenkin_profile"
-#   role = aws_iam_role.jenkins-role.name
-# }
-
-resource "aws_iam_role_policy" "jenkins_ssm" {
-  name = "jenkins-ssm-policy"
-  role = aws_iam_role.jenkins-role.id
-
-  policy = jsonencode({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
 
     Statement = [
       {
+        Action = "sts:AssumeRole"
         Effect = "Allow"
 
-        Action = [
-          "ssm:SendCommand",
-          "ssm:GetCommandInvocation",
-          "ssm:ListCommandInvocations",
-          "ssm:DescribeInstanceInformation"
-        ]
-
-        Resource = "*"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
       }
     ]
   })
+
+  tags = {
+    Name = "Pro-Jenkins"
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "jenkins_ssm" {
+resource "aws_iam_role_policy_attachment" "jenkins_ssm_core" {
   role       = aws_iam_role.jenkins-role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "jenkins_profile" {
+  name = "jenkin_profile"
+
+  role = aws_iam_role.jenkins-role.name
 }
